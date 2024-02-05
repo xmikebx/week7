e3f19e91c73c518a5b8981b466cc160fb4642792
xmikebx <xmikebx@gmail.com>
2024-02-05 15:53:33 +0000

    enhancement: add basic server

--- src/server.js
+++ src/server.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.send("healthy");
-});
+app.use("/example", express.static("example"));
+// app.use("/awsum", express.static("awsum"));
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
