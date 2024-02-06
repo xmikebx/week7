6f7bb1ead19b66fb1860bcd1560581b52c067228
xmikebx <xmikebx@gmail.com>
2024-02-06 11:13:44 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-app.use("/awsum", express.static("awsum"));
-app.use("/mostawsum", express.static("mostawsum"));
-
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
